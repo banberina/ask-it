db0885f5aafd81de818c25e85993dbe391ea12b1
banberina <berina.bandic@gmail.com>
2021-01-11 03:30:36 +0100

    setup of frontend routes + navbar, utils, custom jumbotron components for homepage, reactstrap & custom google fonts added.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+import NavBar from "./components/navbar/navbar.component";
+import Homepage from "./pages/home/home.component";
+import LoginPage from "./pages/login/login.component";
+import RegistrationPage from "./pages/registration/registration.component";
+import QuestionPage from "./pages/individual-question /individual-question.component";
+import ProfilePage from "./pages/profile/profile.component";
+import MyQuestionsPage from "./pages/my-questions/my-questions.component";
+import NotFoundPage from "./pages/404/not-found.component";
+
+import { ToastContainer } from "react-toastify";
+
+import { hasValidJwt } from "./utils/jwtValidator";
+
+import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+/* Private routes - if the user is authenticated, render component; otherwise redirect to home page */
+const UserRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      hasValidJwt() === true ? <Component {...props} /> : <Redirect to="/" />
+    }
+  />
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer />
+      <Router>
+      <NavBar />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/register" component={RegistrationPage} />
+          <UserRoute
+            exact
+            path="/question/:questionId"
+            component={QuestionPage}
+          />
+          <UserRoute exact path="/profile/:userId" component={ProfilePage} />
+          <UserRoute exact path="/my/:userId" component={MyQuestionsPage} />
+          <Route path="*" component={NotFoundPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
