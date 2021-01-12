69a8764bb57fb7ad4e32603485bf7e14980a3aab
banberina <berina.bandic@gmail.com>
2021-01-12 23:02:10 +0100

    the name of the logged in user is now visible

--- client/src/components/welcome-jumbotron/jumbotron-logged-in.component.jsx
+++ client/src/components/welcome-jumbotron/jumbotron-logged-in.component.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Jumbotron, Container } from "reactstrap";
 
+import { users } from "../../api/index";
+
+import { checkToken } from "../../utils/utils";
+
+import helpers from "../../utils/helpers";
+
 const JumbotronLoggedIn = () => {
+  const [name, setName] = useState("");
+
+   const fetchUserName = async () => {
+    await users
+      .getOneUser(helpers.decodeToken()._id)
+      .then((res) => setName(res.data.name));
+  };
+
+  useEffect(() => {
+    if (!checkToken()) {
+      window.location.reload();
+    } else {
+      fetchUserName();
+    }
+  }, []);
+ 
   return (
     <div>
       <Jumbotron fluid>
         <Container fluid>
-          <h2 className="special-font-subheader">Welcome back</h2>
+          <h2 className="special-font-subheader">Welcome back, {name}.</h2>
         </Container>
       </Jumbotron>
     </div>
